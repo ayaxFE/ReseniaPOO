685a7695dc7d4f8fc94257fe0644d4b4ab364291
THEfoxde <thefoxde2005@gmail.com>
2025-11-10 16:14:12 -0300

    hago commit de la estructura Spring boot a utilizar
    formo la estructura Sping boot

---
+++ ReseniaPOO/ReseniaPOO/src/main/java/com/example/ReseniaPOO/ReseniaPooApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReseniaPOO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReseniaPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReseniaPooApplication.class, args);
+	}
+
+}
---
+++ ReseniaPOO/ReseniaPOO/src/test/java/com/example/ReseniaPOO/ReseniaPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReseniaPOO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReseniaPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
